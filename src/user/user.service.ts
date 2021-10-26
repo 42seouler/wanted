7212383a0e19841d68ecdb50a2e06e88b4107c51
42seouler <77034008+42seouler@users.noreply.github.com>
2021-10-26 16:32:24 +0900

    sqlite 동작 확인
    typeorm sqlite3 작동 확인

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  create(createUserDto: any) {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+}
